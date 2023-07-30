22b7ff2f3e87e698bfefe53efd9585da73031420
Gescof <gescolarfernandez@gmail.com>
2023-07-30 19:10:34 +0200

    Modify create method to return HTTP 201 and delete method to return HTTP 204

--- src/main/java/com/gescof/springboot3reactive/controllers/TodoController.java
+++ src/main/java/com/gescof/springboot3reactive/controllers/TodoController.java
@@ -6,6 +6,7 @@ import com.gescof.springboot3reactive.models.dtos.UpdateTodoRequest;
 import com.gescof.springboot3reactive.services.TodoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -26,6 +28,7 @@ public class TodoController {
     private final TodoService todoService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Mono<TodoResponse> create(@RequestBody final CreateTodoRequest createTodoRequest) {
         log.info("POST /todos");
         return todoService.create(createTodoRequest);
@@ -50,6 +53,7 @@ public class TodoController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> delete(@PathVariable final Integer id) {
         log.info("DELETE /todos/{}", id);
         return todoService.deleteById(id);
